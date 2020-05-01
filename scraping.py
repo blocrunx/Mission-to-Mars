ae407e69eacc8ba5e646d19c7740c1d7e746f53d
blocrunx <61406569+blocrunx@users.noreply.github.com>
2020-05-01 15:49:45 -0400

    initial commit for index.html

--- scraping.py
+++ scraping.py
@@ -11,14 +11,15 @@ def scrape_all():
    # Initiate headless driver for deployment
    browser = Browser("chrome", executable_path="chromedriver", headless=False)
    news_title, news_paragraph = mars_news(browser)
-    Run all scraping functions and store results in dictionary
-    data = {
-        "news_title": news_title,
-        "news_paragraph": news_paragraph,
-        "featured_image": featured_image(browser),
-        "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
-    }
+   
+   # Run all scraping functions and store results in dictionary
+   data = {
+      "news_title": news_title,
+      "news_paragraph": news_paragraph,
+      "featured_image": featured_image(browser),
+      "facts": mars_facts(),
+      "last_modified": dt.datetime.now()
+   }
 
 # Path to chromedriver
 # executable_path = {'executable_path': 'chromedriver'}
